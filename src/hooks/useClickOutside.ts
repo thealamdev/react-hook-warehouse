24e10014f6e54aff06f512a96a8c1b78f2d40a74
Md. Shah Alam <shahalam141072@gmail.com>
2026-03-12 09:38:06 +0600

    :pencil2: => Added a new hook useClickOutside

---
+++ src/hooks/useClickOutside.ts
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+
+interface PageProps {
+    ref: React.RefObject<HTMLDivElement>;
+    callback: () => void;
+}
+
+export function useClickOutside({
+    ref,
+    callback
+}: PageProps) {
+    useEffect(() => {
+        const handler = (e: MouseEvent) => {
+            if (ref.current && !ref.current.contains(e.target as Node)) {
+                callback();
+            }
+        }
+        document.addEventListener('mousedown', handler);
+
+        return () => {
+            document.removeEventListener('mousedown', handler);
+        }
+    }, [ref, callback])
+}
